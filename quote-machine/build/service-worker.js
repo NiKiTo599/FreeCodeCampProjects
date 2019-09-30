7818f0a1a9b961b613ce6fdcf894949e354303dd
NiKiTo599 <akulichnikita0@gmail.com>
2019-09-30 12:06:08 +0300

    asd

--- quote-machine/build/service-worker.js
+++ quote-machine/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2200a29af99f87bfd550be91802aa94a.js"
+  "/FreeCodeCampProjects/quote-machine/precache-manifest.9dba5c9c18cd58b5b223014b44c657ca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FreeCodeCampProjects/quote-machine/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
