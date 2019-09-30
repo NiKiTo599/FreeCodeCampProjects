7b73561b239b35978719cdac4aafe3baf4b3344a
NiKiTo599 <akulichnikita0@gmail.com>
2019-09-30 11:56:16 +0300

    Updates

---
+++ precache-manifest.2200a29af99f87bfd550be91802aa94a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04818cd005e2f24ad5a4d2a22936dc23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc645ee9c3df3f589867",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "90ef12b425daca42c6c5",
+    "url": "/static/js/2.6b819420.chunk.js"
+  },
+  {
+    "revision": "cc645ee9c3df3f589867",
+    "url": "/static/js/main.4786427d.chunk.js"
+  },
+  {
+    "revision": "400fa97f935fc235b723",
+    "url": "/static/js/runtime-main.4366493e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
