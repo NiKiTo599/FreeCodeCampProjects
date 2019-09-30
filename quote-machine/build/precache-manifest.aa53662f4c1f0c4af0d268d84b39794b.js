3d24c430254868f944094c956166d0c6c62c14c5
NiKiTo599 <akulichnikita0@gmail.com>
2019-09-30 11:39:00 +0300

    feat: change gitignore

---
+++ quote-machine/build/precache-manifest.aa53662f4c1f0c4af0d268d84b39794b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0746416daffb1609dd08ff770df3532",
+    "url": "/index.html"
+  },
+  {
+    "revision": "635db02ed63720354bdc",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "289bd2ca339dd8a20b77",
+    "url": "/static/js/2.be2bab68.chunk.js"
+  },
+  {
+    "revision": "635db02ed63720354bdc",
+    "url": "/static/js/main.636223ff.chunk.js"
+  },
+  {
+    "revision": "400fa97f935fc235b723",
+    "url": "/static/js/runtime-main.4366493e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
